bcdcdea882b030a676444453e4655d3bd7103c90
Diego Garcia <diegog368@gmail.com>
2020-06-22 20:02:54 -0300

    estas son las bases

---
+++ curso12/curso12laravel/database/factories/ClinicaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Clinica;
+use Faker\Generator as Faker;
+
+$factory->define(Clinica::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
