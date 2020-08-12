e8dbbcaaaa4e984b4d9755ec952b96357074653b
Diego Garcia <diegog368@gmail.com>
2020-08-11 22:54:58 -0300

    poner links del sistema en el welcome blade

--- curso17/curso17laravel/resources/views/welcome.blade.php
+++ curso17/curso17laravel/resources/views/welcome.blade.php
@@ -90,13 +90,8 @@
                 @endforeach
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="clinicas">Clínicas</a>
+                    <a href="roles">Roles</a>
                 </div>
             </div>
         </div>
